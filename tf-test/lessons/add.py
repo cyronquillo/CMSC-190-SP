305245cfef4fa5f7ca02575b7b0d561c2fa48d2e
cyronquillo <cbronquillo@up.edu.ph>
2018-03-20 21:27:51 +0800

    fixed directory for tf-test;

--- tf-test/lessons/add.py
+++ tf-test/lessons/add.py
@@ -3,8 +3,8 @@ import tensorflow as tf
 a = tf.constant([2])
 b = tf.constant([3])
 
-c = tf.add(a,b)
+c = tf.add(a, b)
 
 with tf.Session() as session:
-	result = session.run(c)
-	print(result)
+    result = session.run(c)
+    print(result)
