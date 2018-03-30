dfb961f0c2d49de6139fb106683955310ff37137
cyronquillo <cbronquillo@up.edu.ph>
2018-03-31 02:09:19 +0800

    code for retrieving ground truth;

--- server/utils/data_generate_target.py
+++ server/utils/data_generate_target.py
@@ -1,30 +1,32 @@
 import requests
+import urllib.parse
 
+def get_score_on_claimbuster(url):  
+    try:  
+        req = requests.get(url)
+        print(req)
+        res = req.json()['results']
+        return res[0]['score']
+    except:
+        print("entered exception")
+        return get_score_on_claimbuster(url)
 
-def get_scores_on_claimbuster(url):
-    scores = []
-    
-    print("processing")
-    req = requests.get(url)
-    req_json = req.json()
-    for result in req_json['results']:
-        scores.append(result['score'])
-    return scores
+def generate_url_extension(delimiter, sentence):
+    url_extension = urllib.parse.quote_plus(sentence)
+    url_extension = url_extension.replace('+', delimiter)
+    return url_extension
 
-def generate_url_extension(delimiter, data_path):
-    url_extension = ""
+def generate_target(delimiter, data_path, url):
     f_read = open(data_path, 'r')
     i = 0
+    scores = []
     for line in f_read:
-        print("adding sentence:", i+1)
+        print("getting score for sentence:" + str(i+1), end=" ")
         i+=1
-        # words = line.split()
-        # for word in words:
-            # url_extension += (word + delimiter)
-        url_extension += line
+        url_extension = generate_url_extension(delimiter, line)
+        scores.append(get_score_on_claimbuster(url + url_extension))
     f_read.close()
-
-    return url_extension
+    return scores
 
 
 threshold = 0.40
@@ -35,16 +37,15 @@ url = 'http://idir-server2.uta.edu:80/factchecker/score_text/'
 data_path = '../data/debates/preprocessed.txt'
 target_output_score_path = '../data/debates/ground_sentence_score.txt'
 target_output_class_path = '../data/debates/ground_sentence_class.txt'
-url_extension = generate_url_extension(delimiter, data_path)
 
-scores = get_scores_on_claimbuster(url+url_extension)
+scores = generate_target(delimiter, data_path, url)
 f_write0 = open(target_output_class_path, 'w')
 f_write1 = open(target_output_score_path, 'w')
 
 for score in scores:
     f_write1.write(str(score) + "\n")
     
-    score_class = 0 if score < threshold else 1
+    score_class = 0 if float(score) < threshold else 1
     f_write0.write(str(score_class) + "\n")
 f_write0.close()
 f_write1.close()
